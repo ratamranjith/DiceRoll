a6e635d42c0482127108bf528ca582d4c6fd04e2
ratamranjith@gmail.com <ratamranjith@gmail.com>
2024-03-22 12:15:18 +0530

    color and functionality update

--- script.js
+++ script.js
@@ -18,41 +18,43 @@ let currentPlayer = 0;
 let isSadImageAdded = false;
 
 clickRoll.addEventListener("click", function () {
-  let number = Math.trunc(Math.random() * 6) + 1;
-  diceImage.classList.remove("hidden");
-  diceImage.src = `images/dice${number}.png`;
+  if (processNames() === true) {
+    let number = Math.trunc(Math.random() * 6) + 1;
+    diceImage.classList.remove("hidden");
+    diceImage.src = `images/dice${number}.png`;
 
-  if (number !== 1) {
-    currentScore[currentPlayer] += number;
-    tscore0.textContent = currentScore[0];
-    tscore1.textContent = currentScore[1];
-    if (isSadImageAdded) {
-      tscore0.removeChild(tscore0.lastChild);
-      tscore1.removeChild(tscore1.lastChild);
-      isSadImageAdded = false;
-    }
-    if (currentScore[currentPlayer] >= 20) {
-      winner.classList.remove("hidden");
-      let playerName = eval(`player${currentPlayer}`);
-      winner.style.color = "#FFFFFF";
-      winner.textContent = `${playerName.textContent} Wins`;
-    }
-  } else {
-    currentScore[currentPlayer] = 0;
-    currentPlayer = currentPlayer === 0 ? 1 : 0;
-    tscore0.textContent = currentScore[0];
-    tscore1.textContent = currentScore[1];
-    if (!isSadImageAdded) {
-      let img = new Image();
-      img.src = "/images/sad.gif";
-      img.width = "64";
-      img.height = "64";
-      if (currentPlayer === 0) {
-        tscore0.appendChild(img);
-      } else {
-        tscore1.appendChild(img);
+    if (number !== 1) {
+      currentScore[currentPlayer] += number;
+      tscore0.textContent = currentScore[0];
+      tscore1.textContent = currentScore[1];
+      if (isSadImageAdded) {
+        tscore0.removeChild(tscore0.lastChild);
+        tscore1.removeChild(tscore1.lastChild);
+        isSadImageAdded = false;
       }
-      isSadImageAdded = true;
+      if (currentScore[currentPlayer] >= 20) {
+        winner.classList.remove("hidden");
+        let playerName = eval(`player${currentPlayer}`);
+        winner.style.color = "#FFFFFF";
+        winner.textContent = `${playerName.textContent} Wins`;
+      }
+    } else {
+      currentScore[currentPlayer] = 0;
+      currentPlayer = currentPlayer === 0 ? 1 : 0;
+      tscore0.textContent = currentScore[0];
+      tscore1.textContent = currentScore[1];
+      if (!isSadImageAdded) {
+        let img = new Image();
+        img.src = "/images/sad.gif";
+        img.width = "64";
+        img.height = "64";
+        if (currentPlayer === 0) {
+          tscore0.appendChild(img);
+        } else {
+          tscore1.appendChild(img);
+        }
+        isSadImageAdded = true;
+      }
     }
   }
 });
@@ -68,13 +70,16 @@ updateScore.addEventListener("click", function () {
 });
 
 function processNames() {
-  let Player1 = document.getElementById("Player1").value;
-  let Player2 = document.getElementById("Player2").value;
-  console.log(Player1);
-  if (Player1 === "" || Player2 === "") {
+  let retVal = false;
+  this.Player1 = document.getElementById("Player1").value;
+  this.Player2 = document.getElementById("Player2").value;
+  if (this.Player1 === "" || this.Player2 === "") {
     alert("Please enter the players name");
+    retVal = false;
   } else {
-    player0.textContent = Player1;
-    player1.textContent = Player2;
+    player0.textContent = this.Player1;
+    player1.textContent = this.Player2;
+    retVal = true;
   }
+  return retVal;
 }
